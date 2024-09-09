b7fb67e596e62e3e23ec4d0f8269c0dd1bd67966
Park, minsu <cmwmss59@gmail.com>
2024-09-09 18:31:38 +0900

    자바 코딩 테스트 연습2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        int a=13;
+        int b=4;
+        System.out.println(a+b);//17
+        System.out.println(a-b);//9
+        System.out.println(a*b);//52
+        System.out.println(a==b);//false
+        System.out.println(a!=b);//true
+        System.out.println(a>b);//true
+        System.out.println(a<b);//false
+        System.out.println(a>=b);//true
+        System.out.println(a<=b);//false
+        // 이 이후 부터는 정수형 비트 연산
 
     }
 }
